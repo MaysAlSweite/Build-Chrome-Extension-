55ccd07c5cf058114f37e7f0d105bd7ded098f1a
Mays <mayssweite@gmail.com>
2025-05-17 19:07:04 +0300

    Added Saved Inputs display

--- src/script.js
+++ src/script.js
@@ -2,10 +2,11 @@ const input=document.getElementById("input");
 const saveInput = document.getElementById("saveInput");
 const saveTab = document.getElementById("saveTab");
 const LS_leads = JSON.parse(localStorage.getItem("myLeads"))
+const ul = document.getElementById("ul");
 let myLeads = []
 if (LS_leads) {
     myLeads = LS_leads
-    
+    render(myLeads)
 }
 
 
@@ -13,7 +14,7 @@ saveInput.addEventListener("click", function () {
     myLeads.push(input.value)
     input.value = ""
     localStorage.setItem("myLeads", JSON.stringify(myLeads))
-    
+    render(myLeads)
 })
 
 
@@ -21,6 +22,21 @@ saveTab.addEventListener("click", function () {
     chrome.tabs.query({ active: true, currentWindow: true }, function (tabs) {
         myLeads.push(tabs[0].url)
         localStorage.setItem("myLeads", JSON.stringify(myLeads))
-        
+        render(myLeads)
     })
 })
+
+
+function render(leads) {
+    let listItems = ""
+    for (let i = 0; i < leads.length; i++) {
+        listItems += `
+            <li>
+                <a target='_blank' href='${leads[i]}'>
+                    ${leads[i]}
+                </a>
+            </li>
+        `
+    }
+    ul.innerHTML = listItems
+}
